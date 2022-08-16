d8f65bb41c6f49d721997fa247ed317f69eac7ff
Henrique Louteiro <henrique.louteiro@hotmail.com>
2022-08-16 17:59:36 -0300

    feat: product box

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div>
+    
     <router-view/>
-  </div>
+    </div>
 </template>
 
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+
 <style>
+body, h1, h2, ul, li, p {
+  padding: 0px;
+  margin: 0px;
+}
+
+ul {
+  list-style: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: #78afaf;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 0 80px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.container { 
+  max-width: 900px;
+  padding: 20px 0;
+  margin: 0 auto;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,120 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="card-container container">
+      <div v-for="(produto, index) in produtos" :key="index" class="card">
+        <div class="card-body">
+          <div class="h2 mb-0">
+            <b-icon
+              icon="heart-fill"
+              class="rounded p-2 icon-heart-bg action-produto"
+              variant="danger"
+            ></b-icon>
+          </div>
+
+          <div class="card-container-img">
+            <img :src="produto.image" alt="Card image cap" />
+          </div>
+
+          <div class="card-container-titulo">
+            <p>{{ produto.title }}</p>
+          </div>
+
+          <div class="card-container-titulo d-flex justify-content-between">
+            <div class="d-flex flex-column">
+              <span>Price:</span>
+              <strong>{{ produto.price | numeroPreco}}</strong>
+            </div>
+            <div class="h2 mb-0">
+              <b-iconstack @click="testando()" class="action-produto icon-plus-bg">
+                <b-icon stacked icon="plus"></b-icon>
+              </b-iconstack>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    },
+  },
+  data() {
+    return {
+      produtos: [],
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("https://fakestoreapi.com/products")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+          console.log(this.produtos);
+        });
+    },
+    testando() {
+      console.log("testando");
+    },
+  },
+  mounted() {
+    this.getProdutos();
+  },
+};
 </script>
+
+<style scoped>
+/* CARD */
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  padding: 1rem;
+  gap: 1rem;
+}
+
+.card {
+  border-radius: 35px;
+}
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.card-container-img {
+  height: 150px;
+}
+.card-container-img img {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+}
+
+.card-container-titulo p {
+  font-size: 15px;
+  font-weight: 500;
+}
+
+/* BOTOES */
+.action-produto{
+  cursor: pointer;
+}
+
+/* ICONS */
+.icon-heart-bg {
+  
+  background: rgb(255, 192, 192);
+}
+
+.icon-plus-bg {
+  border-radius: 10px;
+  background: rgb(192, 192, 192);
+}
+</style>
