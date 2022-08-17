59cd573646ffc78e0f9ee249e118c64eb733959f
Henrique Louteiro <henrique.louteiro@hotmail.com>
2022-08-16 21:05:10 -0300

    feat: navbar header

---
+++ src/components/NavBarMarket.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header mb-2 header_nav">
+    <nav class="navbar navbar-expand-lg d-flex flex-column justify-content-center align-items-center  "> 
+      <img class="navbar-brand" src="../assets/logo.png" alt="" width="70px" height="70px">
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavBarMarket",
+};
+</script>
+
+<style scoped>
+.header {
+  background: #ffffff;
+}
+
+.header_nav{
+    box-shadow: 0px 6px 13px 0px rgb(0 0 0 / 20%);
+}
+
+</style>
