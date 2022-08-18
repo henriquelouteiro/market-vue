8e8c7577960bf37bd463a44959772e926f7f3ae0
Henrique Louteiro <henrique.louteiro@hotmail.com>
2022-08-18 20:30:34 -0300

    refactor: cart global

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,34 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    carrinho: [],
   },
-  getters: {
-  },
+  getters: {},
+  methods: {},
   mutations: {
+    //carrinho
+    setCarrinho(state, produto) {
+      state.carrinho.push({
+        idProduto: produto.id,
+        nome: produto.title,
+        preco: produto.price,
+        url: produto.image,
+      });
+      localStorage.setItem("carrinho", JSON.stringify(state.carrinho));
+    },
+    removeCarrinho(state, produto) {
+      state.carrinho.splice(produto, 1);
+      localStorage.setItem("carrinho", JSON.stringify(state.carrinho));
+    },
+    clearCarrinho(state) {
+      state.carrinho = [];
+      localStorage.setItem("carrinho", JSON.stringify(state.carrinho));
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
